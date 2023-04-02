4b3f9654bfee68a85835909d9a4a96967f6853bf
d2nb <824331661@qq.com>
2023-04-02 23:57:19 +0800

    初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
+    'react/jsx-one-expression-per-line': [2, { allow: 'literal' }],
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2]
+  }
+}
